2231546733b3cc82bf0c9cdc53310c9bfc6b827b
huffmanks <huffmanks2@gmail.com>
2022-09-12 20:34:30 -0400

    sidebar expanded false on mobile

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,12 +1,17 @@
 import { createContext, useContext, useRef, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 
+import useWindowSize from '../hooks/useWindowSize'
+
 const GlobalContext = createContext({})
 
 const GlobalContextProvider = ({ children }) => {
     const searchInputRef = useRef(null)
 
-    const [sideBarExpanded, setSideBarExpanded] = useState(true)
+    const { width } = useWindowSize()
+    console.log(width)
+
+    const [sideBarExpanded, setSideBarExpanded] = useState(width > 600 ? true : false)
 
     useHotkeys('ctrl+k, cmd+k', (e) => {
         e.preventDefault()
---
+++ src/hooks/useWindowSize.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+const getWindowSize = () => {
+    const { innerWidth: width, innerHeight: height } = window
+    return {
+        width,
+        height,
+    }
+}
+
+const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState(getWindowSize())
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize(getWindowSize())
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    return windowSize
+}
+
+export default useWindowSize
